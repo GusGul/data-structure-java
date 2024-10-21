3f0ea84428de12c52b732c06eea89f43202e6e45
GusGul <gustavosalgadolima@gmail.com>
2024-10-21 17:14:48 -0300

    feat: add selection sort algorithm

--- SortAlgorithms/SelectionSort.java
+++ SortAlgorithms/SelectionSort.java
@@ -1,11 +1,28 @@
 package SortAlgorithms;
 
-import Utils.SearchMin;
+import java.util.Arrays;
 
 public class SelectionSort {
-    public static int[] selectionSort(int[] arr) {
-        int min_index = SearchMin.searchMin(arr);
-        
-        return arr;
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 3, 1, 4};
+        System.out.println("Array original: " + Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("Array ordenado: " + Arrays.toString(arr));
     }
 }
